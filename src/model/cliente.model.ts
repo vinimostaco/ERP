ff4849d5ae17920400a32aa02895ac328f87acaf
viniciusmostacoalest <vinicius.mostaco@alest.com.br>
2025-02-08 20:43:49 -0300

    feat: create method to create user based on sheet

---
+++ src/model/cliente.model.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const clientSchema = new mongoose.Schema(
+  {
+    id: { type: mongoose.Schema.Types.ObjectId },
+    nome: { type: String, required: true },
+    cpf: { type: String },
+    celular: { type: String },
+    criar_conta: { type: String },
+    plano_id: { type: String },
+  },
+  { versionKey: false }
+);
+
+const client = mongoose.model("Dados", clientSchema);
+
+export { client, clientSchema };
