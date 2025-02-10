99e7f869560e3f14abc476cdddfc6844f62c7691
viniciusmostacoalest <vinicius.mostaco@alest.com.br>
2025-02-10 20:16:36 -0300

    refactor: change functional to poo

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,12 +1,11 @@
 import { request, Request, Response } from "express";
-import { registerClient, updateClient } from "../services/admin.service";
-import { searchFullBase } from "../services/admin.service";
-import { excelToJson } from "../utils/excelToJson";
+import { ClientService } from "../services/admin.service";
+import { FileUtils } from "../utils/excelToJson";
 
 export class AdminController {
   static async createUser(req: Request, res: Response) {
     try {
-      const clients = await excelToJson(req.body.sheetName);
+      const clients = await FileUtils.excelToJson(req.body.sheetName);
       clients?.forEach(async (client: any) => {
         const clientData = {
           nome: client.NOME_CLIENTE,
@@ -15,7 +14,7 @@ export class AdminController {
           criar_conta: client.CRIAR_CONTA,
           plano_id: client.PLANO_ID,
         };
-        await registerClient(clientData);
+        await ClientService.registerClient(clientData);
       });
       res.status(200).json({
         status: 200,
@@ -28,7 +27,7 @@ export class AdminController {
 
   static async returnFullBase(_: any, res: Response) {
     try {
-      const data = await searchFullBase();
+      const data = await ClientService.searchFullBase();
       res.send(data);
     } catch (err) {
       console.error(`Erro ao listar base: ${err}`);
@@ -37,7 +36,7 @@ export class AdminController {
 
   static async findClient(req: Request, res: Response) {
     try {
-      const update = await updateClient(req.body);
+      const update = await ClientService.updateClient(req.body);
       res.send("tudo certo, atualizado");
     } catch (err) {
       console.error(`Erro ao atualizar cliente: ${err}`);
