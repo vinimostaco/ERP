8ffc3fc7577e6efad46354a91a7b8bdd80810489
viniciusmostacoalest <vinicius.mostaco@alest.com.br>
2025-02-14 21:57:23 -0300

    feat: add cors configuration to communicate with front

--- crm back v1/src/app.ts
+++ crm back v1/src/app.ts
@@ -1,25 +1,25 @@
 import express from "express";
-import adminRoute from "./routes/routes"
-
+import adminRoute from "./routes/routes";
+import cors from "cors";
 import MongooseConnect from "./config/dbConnect";
 
-(async()=>{
+(async () => {
   const mongooseConnect = new MongooseConnect();
 
-const connect = await mongooseConnect.databaseConnect();
+  const connect = await mongooseConnect.databaseConnect();
 
-connect.on("error", () => {
-  console.log("Error in connecting to database ❌");
-});
+  connect.on("error", () => {
+    console.log("Error in connecting to database ❌");
+  });
 
-connect.on("open", () => {
-  console.log("Connected with database ✅");
-});
-})()
+  connect.on("open", () => {
+    console.log("Connected with database ✅");
+  });
+})();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-app.use("/admin", adminRoute)
+app.use("/admin", adminRoute);
 
 export default app;
