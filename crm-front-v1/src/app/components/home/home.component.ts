b6cad19efd0b755d281f7fad73f97bfc5f21fd43
viniciusmostacoalest <vinicius.mostaco@alest.com.br>
2025-02-14 21:56:54 -0300

    feat: method to getClients

--- crm-front-v1/src/app/components/home/home.component.ts
+++ crm-front-v1/src/app/components/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import axios from 'axios';
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  submit() {
+    console.log('Formulário enviado!');
+  }
 
+  async getClients() {
+    const url = 'http://localhost:3000/admin/fullClients';
+    const res = await axios.get(url);
+    console.log(res.data);
+  }
 }
